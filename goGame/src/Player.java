b66ed633abc3fce30e9bc19396d4de0a89a94daf
pdstarz01 <elitesorenea1@gmail.com>
2024-04-01 11:28:29 -0600

    Changes

--- goGame/src/Player.java
+++ goGame/src/Player.java
@@ -1,22 +1,24 @@
+import java.util.Scanner;
+
 public class Player {
-    String piece = "";
-    /*public static void PlayerTurn(String player){
-        String piece = "";
-        if (player == "Black"){piece = Black_Piece;}
-        if (player == "White"){piece = White_Piece;}
+    static Scanner scan;
+    String piece  = "";
+    public static int[] PlayerTurn(){
         System.out.print("What is the X point you want to place a piece?:");
         System.out.println();
         int moveX = scan.nextInt();
         System.out.print("What is the Y point you want to place a piece?:");
         System.out.println();
         int moveY = scan.nextInt();
+        int move[] = new int[]{moveX,moveY};
+        return move;
+        /*
         try {
             Board[moveX-1][moveY-1] = piece;
         }
         catch(Exception e) {
             System.out.print("Invalid Input");
         }
-        PrintBoard();
+        */
     }
-    */
 }
